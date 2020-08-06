149eaf8e3fab0f612bec7a8851183f95b1bb8127
Mamun Abdullah <21096193+thetradecoder@users.noreply.github.com>
2020-08-07 01:46:25 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // code by: Mamun Abdullah (@theTradeCoder)
-$('#googleSearch').submit((e)=>{
-  e.preventDefault();
-  let searchTopic = $('.google-search').val();
-  searchTopic = searchTopic.split(' ').join('+');
-  let searchUrl = `https://www.google.com/search?q=${searchTopic}&oq=${searchTopic}&ie=UTF-8`;
- window.open(searchUrl);
+$(document).ready(()=>{
+
+    $('#googleSearch').submit((e)=>{
+      e.preventDefault();
+      let searchTopic = $('.google-search').val();
+      searchTopic = searchTopic.split(' ').join('+');
+      let searchUrl = `https://www.google.com/search?q=${searchTopic}&oq=${searchTopic}&ie=UTF-8`;
+     window.open(searchUrl);
+    });
+  
 });
